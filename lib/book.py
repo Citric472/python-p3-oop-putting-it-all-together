bb313ea4b9fd6b24458f5293b2efa527d3624e56
=Memo <=chepkemoicynthia4@gmail.com>
2024-03-02 20:39:19 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None  # Private variable for page_count
+        self.page_count = page_count  # Using the setter method to set the initial page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+# Example usage:
+book1 = Book("And Then There Were None", 272)
+print(book1.title)         # Output: And Then There Were None
+print(book1.page_count)    # Output: 272
+
+book1.turn_page()          # Output: Flipping the page...wow, you read fast!
+
+
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None  # Private variable for size
+        self.size = size  # Using the setter method to set the initial size
+        self.condition = "Used"  # Default condition is Used
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set the condition to New after repair
+
+    
